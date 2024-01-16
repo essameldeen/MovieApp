08e8acbf797bc92d3c0a35040b5160705867c732
Egypt <essam.mohamed@mdi.com.eg>
2024-01-16 21:36:52 +0200

    delete api key

--- app/src/main/java/com/task/movieapp/utils/constant.kt
+++ app/src/main/java/com/task/movieapp/utils/constant.kt
@@ -1,10 +1,8 @@
 package com.task.movieapp.utils
 
 object Constant {
-    const val APIKEY = "96055e6ff0b239d3fd04351263c06b94"
-    const val APIREADACCESSTOKEN =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjA1NWU2ZmYwYjIzOWQzZmQwNDM1MTI2M2MwNmI5NCIsInN1YiI6IjVhMWI0MjJiYzNhMzY4MGI4YTA0NTg1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8HkSd9T9iXW6L5KSvDCfDqpshmCG31EOWwcBNPUeZy8"
-
+    const val APIKEY = "your api key"
+    const val APIREADACCESSTOKEN = "your token"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
